4672898a0d11aaada7435f840012f0c617a79044
hakancaliskann22 <166514582+hakancaliskann22@users.noreply.github.com>
2026-03-11 13:56:32 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T10:56:26.878Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-11T10:56:32.530Z
+Commit: Add export functionality for reports
