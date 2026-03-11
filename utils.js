7c3c3b02577e6d41532593806aa91e0ecce0fa37
hakancaliskann22 <166514582+hakancaliskann22@users.noreply.github.com>
2026-03-11 13:56:26 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T10:56:15.870Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-11T10:56:26.878Z
+Commit: Resolve null pointer exception in user service
